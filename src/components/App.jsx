a5aab49cb73515dfc4c5ed008226316da61244cc
andrii-pozniak <andriipozniak@gmail.com>
2022-11-05 23:34:20 +0200

    add fetch and loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-// import { ImageGallery } from "./ImageGallery/ImageGallery";
+import  ImageGallery  from "./ImageGallery/ImageGallery";
 // import {  ImageGalleryItem} from "./ImageGalleryItem/ImageGalleryItem";
 import SearchBar  from "components/SearchBar/SearchBar";
 // import { Loader } from "./Loader/Loader";
@@ -7,12 +7,10 @@ import SearchBar  from "components/SearchBar/SearchBar";
 // import { Modal } from "./Modal/Modal";
 
 export class App extends Component {
-  q = 'flowers'
+ 
   state = {
     imageName: '',
-   photo: [],
-  //  q: 'flowers',
-   page: 1,
+   
   }
   handleFormSubmit = imageName => {
     this.setState({imageName})
@@ -23,11 +21,11 @@ export class App extends Component {
     return (
       <>
       <SearchBar onSubmit={this.handleFormSubmit} />
-      {/* <ImageGallery/>
-      <ImageGalleryItem/>
+       <ImageGallery imageName={this.state.imageName}/> 
+      {/* <ImageGalleryItem />
       <Loader/>
       <Button/>
-      <Modal/> */}
+      <Modal/> */} 
       </>
       
 
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -6,26 +6,29 @@ import  s  from "components/ImageGallery/ImageGallery.module.css";
 const BASE_URL = `https://pixabay.com/api/`
 
 export default class ImageGallery extends Component {
+    state = {
+        images: null,
+        isLoading: false
+    }
     
-    async componentDidMount () {
-        // const option = {
-        //   key: `30111501-80dfaf6bf0e872b32b653e61a`,
-              
-        //   image_type: `photo`,
-        //   orientation: `horizontal`,
-        //   safesearch: true
-        //  }
-        const response = await axios.get(`${BASE_URL}?key=30111501-80dfaf6bf0e872b32b653e61a&q=${this.q}&image_type=photo`);
-        this.setState({photo: response.data.hits});
-        // fetch(`${BASE_URL}?key=30111501-80dfaf6bf0e872b32b653e61a&q=flowers&image_type=photo`)
-        // .then(res => res.json()).then(photo => this.setState({photo: photo.data.hits}));
+    async componentDidUpdate (prevPops, prevState) {
+        
+       
+        if(prevPops.imageName !== this.props.imageName) {
+            this.setState({isLoading: true})
+            const response = await axios.get(`${BASE_URL}?key=30111501-80dfaf6bf0e872b32b653e61a&q=${this.props.imageName}&image_type=photo`);
+        this.setState({images: response.data.hits, isLoading: false, });
+        }
         
       }
 
     render() {
-        return <ul className={s.ImageGallery}>
+        const {isLoading} = this.state;
+        return ( <><div>{isLoading && <p>Loading...</p> }</div>
+        <ul className={s.ImageGallery}>
         <li></li>
-       </ul>
+       </ul></>)
+        
     }
 
 };
